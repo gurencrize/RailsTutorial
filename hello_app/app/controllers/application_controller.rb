f3bf8ba917ce05ff1119df9e085df1fc7c4e7fea
crize <34015092+gurencrize@users.noreply.github.com>
2023-03-07 08:21:55 +0900

    Update application_controller.rb
    router試す用&課題の回答を追加

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    def hello
+        render html: "¡Hola, mundo!"
+    end
+
+    def aaaa
+        render html: "aaaaaa"
+    end
+
+    def goodbye
+        render html: "goodbye, world!"
+    end
 end
